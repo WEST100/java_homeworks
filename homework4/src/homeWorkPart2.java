66dfb7fb19b843bec0bf83ba428f56cf6bdcc460
Denys Semeniv <142745696+WEST100@users.noreply.github.com>
2023-12-05 21:03:37 +0100

    Add files via upload

---
+++ homework4/src/homeWorkPart2.java
@@ -0,0 +1,34 @@
+public class homeWorkPart2 {
+    public static void getSum(int num1, int num2) {
+        System.out.println("getSum from Operation: " + (num1 + num2));
+    }
+
+    public int getPlus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void getMin(int num1, int num2) {
+        System.out.println("getMin from Operation: " + (num1 - num2));
+    }
+
+    public int getMinus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static void getMult(int num1, int num2) {
+        System.out.println("getMult from Operation: " + (num1 * num2));
+    }
+
+    public int getMultiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void getDiv(int num1, int num2) {
+        System.out.println("getDiv from Operation: " + (num1 / num2));
+    }
+
+    public int getDivision(int num1, int num2) {
+        return num1 / num2;
+    }
+}
+
